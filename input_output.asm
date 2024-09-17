d64df231581923d9e833fafc0c1ff79aa3fb9caf
Sajedullah Aref <152142322+rf104@users.noreply.github.com>
2024-09-17 19:44:58 +0600

    Create input_output.asm

---
+++ input_output.asm
@@ -0,0 +1,18 @@
+.MODEL SMALL
+.STACK 100H
+.CODE
+MAIN PROC
+    MOV AH,1  ; TAKE INPUT
+    INT 21H
+    MOV BL,AL
+    
+    MOV AH,2   ;SHOW OUTPUT
+    MOV DL,BL
+    INT 21H 
+    
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+    
+    MAIN ENDP
+END MAIN
